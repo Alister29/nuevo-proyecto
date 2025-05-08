3fcb70584d2c266b9e2def06473a6f540541aa65
Alister29 <101656907+Alister29@users.noreply.github.com>
2025-05-07 21:12:09 -0400

    carga las materias

---
+++ context/scheduleData.js
@@ -0,0 +1,435 @@
+const scheduleData = {
+    "A": [
+      {
+        nombre: "Álgebra I",
+        docentes: [
+          {
+            grupo: "G: 10",
+            nombre: "RODRIGUEZ SEJAS JUAN ANTONIO",
+            horarios: [
+              { dia: "Martes", hora: "09:45-11:15", aula: "693B" },
+              { dia: "Miercoles", hora: "08:15-09:45", aula: "692E" },
+              { dia: "Viernes", hora: "09:45-11:15", aula: "692F" }
+            ]
+          },
+          {
+            grupo: "G: 14",
+            nombre: "LEON ROMERO GUALBERTO",
+            horarios: [
+              { dia: "Martes", hora: "15:45-17:15", aula: "692E" },
+              { dia: "Jueves", hora: "11:15-12:45", aula: "692E" },
+              { dia: "Sabado", hora: "08:15-09:45", aula: "691E" }
+            ]
+          },
+          {
+            grupo: "G: 5",
+            nombre: "TABORGA ACHA FIDEL",
+            horarios: [
+              { dia: "Miercoles", hora: "14:15-15:45", aula: "617" },
+              { dia: "Viernes", hora: "08:15-09:45", aula: "617C" },
+              { dia: "Sabado", hora: "08:15-09:45", aula: "692B" }
+            ]
+          },
+          {
+            grupo: "G: 6",
+            nombre: "PATINO TITO RONALD EDGAR",
+            horarios: [
+              { dia: "Lunes", hora: "20:15-21:45", aula: "622" },
+              { dia: "Martes", hora: "20:15-21:45", aula: "692C" },
+              { dia: "Miercoles", hora: "20:15-21:45", aula: "623" }
+            ]
+          },
+          {
+            grupo: "G: 8",
+            nombre: "LEON ROMERO GUALBERTO",
+            horarios: [
+              { dia: "Miercoles", hora: "11:15-12:45", aula: "691C" },
+              { dia: "Viernes", hora: "06:45-08:15", aula: "692F" },
+              { dia: "Jueves", hora: "06:45-08:15", aula: "625D" }
+            ]
+          },
+          {
+            grupo: "G: 9",
+            nombre: "POR DESIGNAR DOCENTE",
+            horarios: [
+              { dia: "Lunes", hora: "14:15-15:45", aula: "693A" },
+              { dia: "Martes", hora: "15:45-17:15", aula: "660" },
+              { dia: "Jueves", hora: "12:45-14:15", aula: "661" }
+            ]
+          }
+          
+        ]
+      },
+      {
+        nombre: "Cálculo I",
+        docentes: [
+          {
+            grupo: "G: 10",
+            nombre: "POR DESIGNAR DOCENTE",
+            horarios: [
+              { dia: "Miercoles", hora: "20:15-21:45", aula: "693B" },
+              { dia: "Viernes", hora: "20:15-21:45", aula: "623" },
+              { dia: "Sabado", hora: "09:45-11:15", aula: "617C" }
+            ]
+          },
+          {
+            grupo: "G: 11",
+            nombre: "ROJAS ZURITA RAMIRO",
+            horarios: [
+              { dia: "Lunes", hora: "06:45-08:15", aula: "622" },
+              { dia: "Martes", hora: "08:15-09:45", aula: "692B" },
+              { dia: "Jueves", hora: "08:15-09:45", aula: "692E" }
+            ]
+          },
+          {
+            grupo: "G: 12",
+            nombre: "DELGADILLO COSSIO DAVID ALFREDO",
+            horarios: [
+              { dia: "Lunes", hora: "18:45-20:15", aula: "690B" },
+              { dia: "Martes", hora: "18:45-20:15", aula: "625D" },
+              { dia: "Miercoles", hora: "18:45-20:15", aula: "691D" }
+            ]
+          },
+          {
+            grupo: "G: 17",
+            nombre: "OMONTE OJALVO JOSE ROBERTO",
+            horarios: [
+              { dia: "Martes", hora: "11:15-12:45", aula: "642" },
+              { dia: "Miercoles", hora: "09:45-11:15", aula: "691C" },
+              { dia: "Viernes", hora: "09:45-11:15", aula: "692H" }
+            ]
+          },
+          {
+            grupo: "G: 7",
+            nombre: "RODRIGUEZ SEJAS JUAN ANTONIO",
+            horarios: [
+              { dia: "Lunes", hora: "12:45-14:15", aula: "612" },
+              { dia: "Miercoles", hora: "12:45-14:15", aula: "661" },
+              { dia: "Jueves", hora: "09:45-11:15", aula: "692B" }
+            ]
+          },
+          {
+            grupo: "G: 8",
+            nombre: "OMONTE OJALVO JOSE GIL",
+            horarios: [
+              { dia: "Martes", hora: "18:45-20:15", aula: "612" },
+              { dia: "Miercoles", hora: "06:45-08:15", aula: "617C" },
+              { dia: "Sabado", hora: "11:15-12:45", aula: "692E" }
+            ]
+          },
+          {
+            grupo: "G: 9",
+            nombre: "JUCHANI BAZUALDO DEMETRIO",
+            horarios: [
+              { dia: "Lunes", hora: "12:45-14:15", aula: "651" },
+              { dia: "Miercoles", hora: "12:45-14:15", aula: "690D" },
+              { dia: "Jueves", hora: "11:15-12:45", aula: "622" }
+            ]
+          }
+        ]
+      },
+      {
+        nombre: "Fisica General",
+        docentes: [
+          {
+            grupo: "G: C",
+            nombre: "FLORES FLORES FREDDY",
+            horarios: [
+              { dia: "Lunes", hora: "14:15-15:45", aula: "617" },
+              { dia: "Viernes", hora: "17:15-18:45", aula: "693B" }
+            ]
+          },
+          {
+            grupo: "G: C1",
+            nombre: "SHITIKOV GAGARINA GALINA",
+            horarios: [
+              { dia: "Martes", hora: "11:15-12:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: C2",
+            nombre: "TERRAZAS VARGAS JUAN CARLOS",
+            horarios: [
+              { dia: "Viernes", hora: "08:15-09:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: C3",
+            nombre: "MOREIRA CALIZAYA RENE",
+            horarios: [
+              { dia: "Martes", hora: "11:15-12:45", aula: "621" }
+            ]
+          },
+          {
+            grupo: "G: C4",
+            nombre: "CASTRO LAZARTE CECILIA BEATRIZ",
+            horarios: [
+              { dia: "Lunes", hora: "14:15-15:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: C5",
+            nombre: "CASTRO LAZARTE CECILIA BEATRIZ",
+            horarios: [
+              { dia: "Jueves", hora: "17:15-18:45", aula: "684L8" }
+            ]
+          },
+          {
+            grupo: "G: C6",
+            nombre: "ZAMURIANO CARBAJAL JUAN MARCELO",
+            horarios: [
+              { dia: "Lunes", hora: "17:15-18:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: C7",
+            nombre: "FLORES FLORES FREDDY",
+            horarios: [
+              { dia: "Martes", hora: "14:15-15:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: D",
+            nombre: "FUENTES MIRANDA IVAN",
+            horarios: [
+              { dia: "Miercoles", hora: "06:45-08:15", aula: "691B" },
+              { dia: "Viernes", hora: "12:45-14:15", aula: "612" }
+            ]
+          },
+          {
+            grupo: "G: D1",
+            nombre: "UGARTE CEJAS FELIX",
+            horarios: [
+              { dia: "Martes", hora: "15:45-17:15", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: D2",
+            nombre: "ZAMURIANO CARBAJAL JUAN MARCELO",
+            horarios: [
+              { dia: "Jueves", hora: "14:15-15:45", aula: "621" }
+            ]
+          },
+          {
+            grupo: "G: D3",
+            nombre: "ZAMURIANO CARBAJAL JUAN MARCELO",
+            horarios: [
+              { dia: "Jueves", hora: "15:45-17:15", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: D4",
+            nombre: "FLORES FLORES FREDDY",
+            horarios: [
+              { dia: "Viernes", hora: "15:45-17:15", aula: "618" }
+            ]
+          },
+          {
+            grupo: "G: D5",
+            nombre: "GUZMAN SAAVEDRA ROCIO",
+            horarios: [
+              { dia: "Martes", hora: "14:15-15:45", aula: "618" }
+            ]
+          },
+          {
+            grupo: "G: D6",
+            nombre: "POR DESIGNAR DOCENTE",
+            horarios: [
+              { dia: "Jueves", hora: "08:15-09:45", aula: "621" }
+            ]
+          },
+          {
+            grupo: "G: D7",
+            nombre: "SHITIKOV GAGARINA GALINA",
+            horarios: [
+              { dia: "Martes", hora: "08:15-09:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: E",
+            nombre: "MOREIRA CALIZAYA RENE",
+            horarios: [
+              { dia: "Miercoles", hora: "11:15-12:45", aula: "622" },
+              { dia: "Viernes", hora: "08:15-09:45", aula: "607" }
+            ]
+          },
+          {
+            grupo: "G: E1",
+            nombre: "SHITIKOV GAGARINA GALINA",
+            horarios: [
+              { dia: "Martes", hora: "09:45-11:15", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: E2",
+            nombre: "DAVALOS BROZOVIC JORGE",
+            horarios: [
+              { dia: "Miercoles", hora: "09:45-11:15", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: E3",
+            nombre: "ORDONEZ SALVATIERRA MIGUEL ANGEL",
+            horarios: [
+              { dia: "Lunes", hora: "08:15-09:45", aula: "684L6" }
+            ]
+          },
+          {
+            grupo: "G: E4",
+            nombre: "VASQUEZ CARRILLO MICHAEL HUASCAR",
+            horarios: [
+              { dia: "Martes", hora: "15:45-17:15", aula: "621" }
+            ]
+          },
+          {
+            grupo: "G: E5",
+            nombre: "GUZMAN SAAVEDRA ROCIO",
+            horarios: [
+              { dia: "Jueves", hora: "09:45-11:15", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: E6",
+            nombre: "DAVALOS BROZOVIC JORGE",
+            horarios: [
+              { dia: "Viernes", hora: "15:45-17:15", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: E7",
+            nombre: "DAVALOS BROZOVIC JORGE",
+            horarios: [
+              { dia: "Viernes", hora: "17:15-18:45", aula: "620" }
+            ]
+          },
+          {
+            grupo: "G: E8",
+            nombre: "VASQUEZ CARRILLO MICHAEL HUASCAR",
+            horarios: [
+              { dia: "Viernes", hora: "09:45-11:15", aula: "618" }
+            ]
+          },
+          {
+            grupo: "G: F",
+            nombre: "ORDONEZ SALVATIERRA MIGUEL ANGEL",
+            horarios: [
+              { dia: "Lunes", hora: "09:45-11:15", aula: "691E" },
+              { dia: "Viernes", hora: "09:45-11:15", aula: "622" }
+            ]
+          },
+          {
+            grupo: "G: F1",
+            nombre: "TERRAZAS VARGAS JUAN CARLOS",
+            horarios: [
+              { dia: "Viernes", hora: "09:45-11:15", aula: "621" }
+            ]
+          },
+          {
+            grupo: "G: F2",
+            nombre: "TERRAZAS VARGAS JUAN CARLOS",
+            horarios: [
+              { dia: "Miercoles", hora: "08:15-09:45", aula: "621" }
+            ]
+          },
+          {
+            grupo: "G: F3",
+            nombre: "GUZMAN SAAVEDRA ROCIO",
+            horarios: [
+              { dia: "Lunes", hora: "14:15-15:45", aula: "618" }
+            ]
+          },
+          {
+            grupo: "G: F4",
+            nombre: "GUZMAN SAAVEDRA ROCIO",
+            horarios: [
+              { dia: "Martes", hora: "08:15-09:45", aula: "684L6" }
+            ]
+          }
+        ]
+      },
+      {
+        nombre: "Inglés I",
+        docentes: [
+          {
+            grupo: "G: 1",
+            nombre: "CESPEDES GUIZADA MARIA BENITA",
+            horarios: [
+              { dia: "Martes", hora: "08:15-09:45", aula: "693B" },
+              { dia: "Viernes", hora: "08:15-09:45", aula: "691D" }
+            ]
+          },
+          {
+            grupo: "G: 2",
+            nombre: "CESPEDES GUIZADA MARIA BENITA",
+            horarios: [
+              { dia: "Martes", hora: "11:15-12:45", aula: "692F" },
+              { dia: "Viernes", hora: "09:45-11:15", aula: "693A" }
+            ]
+          },
+          {
+            grupo: "G: 3",
+            nombre: "PEETERS ILONAA MAGDA LENA",
+            horarios: [
+              { dia: "Lunes", hora: "06:45-08:15", aula: "691B" },
+              { dia: "Miercoles", hora: "06:45-08:15", aula: "692H" }
+            ]
+          },
+          {
+            grupo: "G: 4",
+            nombre: "GRILO SALVATIERRA MARIA ESTELA",
+            horarios: [
+              { dia: "Martes", hora: "15:45-17:15", aula: "692G" },
+              { dia: "Viernes", hora: "14:15-15:45", aula: "692E" }
+            ]
+          },
+          {
+            grupo: "G: 5",
+            nombre: "CESPEDES GUIZADA MARIA BENITA",
+            horarios: [
+              { dia: "Jueves", hora: "09:45-11:15", aula: "692F" },
+              { dia: "Viernes", hora: "11:15-12:45", aula: "691B" }
+            ]
+          }
+          
+        ]
+      }
+    ],
+    "B": [
+      {
+        nombre: "Álgebra II",
+        docentes: [
+          {
+            grupo: "G: 5",
+            nombre: "TAYLOR TERRAZAS DARLONG HOWARD",
+            horarios: [
+              { dia: "Jueves", hora: "08:15-09:45", aula: "622" },
+              { dia: "Viernes", hora: "08:15-09:45", aula: "660"},
+              { dia: "Viernes", hora: "09:45-11:15", aula: "661" }
+            ]
+          },
+          {
+            grupo: "G: 6",
+            nombre: "MEDINA GAMBOA JULIO",
+            horarios: [
+              { dia: "Jueves", hora: "15:45-17:15", aula: "660" },
+              { dia: "Viernes", hora: "12:45-14:15", aula: "691A"},
+              { dia: "Sabado", hora: "08:15-09:45", aula: "692A" }
+            ]
+          },
+          {
+            grupo: "G: 8",
+            nombre: "OMONTE OJALVO JOSE ROBERTO",
+            horarios: [
+              { dia: "Lunes", hora: "11:15-12:45", aula: "624" },
+              { dia: "Jueves", hora: "14:15-15:45", aula: "692B"},
+              { dia: "Martes", hora: "14:15-15:45", aula: "692D" }
+            ]
+          }
+
+        ]
+      }
+    ]
+  };
+  
+  export default scheduleData;
+  
