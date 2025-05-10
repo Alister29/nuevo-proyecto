e9fa65aa0e4a66624c4fa6b8c7360e42f40198ac
Rocio <luizagamebel@gmail.com>
2025-05-09 23:02:42 -0400

    cambios admin

---
+++ screens/admin/DocumentoScreen.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const DocumentoScreen = () => {
+  return (
+    <View>
+      <Text>Admin documento</Text>
+    </View>
+  );
+};
---
+++ screens/admin/MallaScreen.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const MallaScreen = () => {
+  return (
+    <View>
+      <Text>Admin malla</Text>
+    </View>
+  );
+};
