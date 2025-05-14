f0095157b2433ddbcce886c60871d79ace02ce59
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2025-05-14 18:07:20 -0400

    Componente inicial

--- screens/ProgresoScreen.jsx
+++ screens/ProgresoScreen.jsx
@@ -1,10 +1,31 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+import { TarjetaProgreso } from "../components";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: { display: "flex", flexDirection: "row" },
+});
 
 export const ProgresoScreen = () => {
   return (
-    <View>
-      <Text>Hola Progreso</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <ScrollView horizontal>
+          <View style={styles.row}>
+            <TarjetaProgreso />
+            <TarjetaProgreso />
+          </View>
+          <View style={styles.row}>
+            <TarjetaProgreso />
+            <TarjetaProgreso />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
