fdfb2a91fb20ae143f6e605b28fdb255e3556ba4
jhonjc02 <jhonj.colque02@gmail.com>
2025-05-16 17:50:40 -0400

    subir pensum de carrera

--- navigation/RootNavigator.jsx
+++ navigation/RootNavigator.jsx
@@ -35,7 +35,7 @@ export const RootNavigator = () => {
 
   if (isAuthenticated === null) return null;
 
-  const isAdmin = email === "admin@example.com"; // 🛠️ Cambia esto según tu correo de administrador
+  const isAdmin = email === "admin@gmail.com"; // 🛠️ Cambia esto según tu correo de administrador
   console.log("isAdmin:", isAdmin,isAuthenticated);
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
