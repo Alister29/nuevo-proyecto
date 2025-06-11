765998f6a134328d7bfa7c33fc4b0154fb1a3029
Alister29 <101656907+Alister29@users.noreply.github.com>
2025-06-11 18:30:34 -0400

    Update scheduleService.js

--- services/scheduleService.js
+++ services/scheduleService.js
@@ -1,20 +1,15 @@
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import { auth, db } from "../database/firebaseConfig";
+import { db } from "../database/firebaseConfig";
 import { COLLECTIONS } from "../database/collections";
-import { useAuth } from "../context/AuthContext";
 
 /**
  * Obtiene el horario guardado del usuario en Firebase.
  */
-async function cargarHorarioDesdeFirebase(setMateriasAsignadas) {
-  const { user } = useAuth(); // Obtener usuario desde el contexto
-
-  if (!user) {
+async function cargarHorarioDesdeFirebase(userId, setMateriasAsignadas) {
+  if (!userId) {
     console.error("❌ Usuario no autenticado.");
     return;
   }
-
-  const userId = user.uid; // Obtener el ID del usuario
 
   try {
     const docSnap = await getDoc(doc(db, "horarios", userId));
@@ -29,15 +24,11 @@ async function cargarHorarioDesdeFirebase(setMateriasAsignadas) {
 /**
  * Guarda el horario seleccionado del usuario autenticado en Firestore.
  */
-async function guardarHorarioEnFirebase(materiasAsignadas) {
-  const { user } = useAuth(); // Obtener usuario desde el contexto
-
-  if (!user) {
+async function guardarHorarioEnFirebase(userId, materiasAsignadas) {
+  if (!userId) {
     console.error("❌ Usuario no autenticado.");
     return;
   }
-
-  const userId = user.uid; // Obtener el ID del usuario
 
   try {
     await setDoc(doc(db, "horarios", userId), {
