0643909f29f5f4891523013856a4869a2f12afc7
carlyggc <carly18us@gmail.com>
2025-06-12 21:47:17 -0400

    Removed large build files from version control

--- android/app/build/intermediates/cxx/Debug/5x6zg4a4/logs/armeabi-v7a/build_command_targets.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-"C:\\Users\\Carla\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Carla\\202\\progra_movil\\nuevo-proyecto\\android\\app\\.cxx\\Debug\\5x6zg4a4\\armeabi-v7a" ^
-  appmodules ^
-  react_codegen_rnpicker ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
--- android/app/build/intermediates/cxx/Debug/5x6zg4a4/logs/x86_64/build_command_targets.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-"C:\\Users\\Carla\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Carla\\202\\progra_movil\\nuevo-proyecto\\android\\app\\.cxx\\Debug\\5x6zg4a4\\x86_64" ^
-  appmodules ^
-  react_codegen_rnpicker ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
